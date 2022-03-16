72a6f669039bc1d9a4c1754005bbe8e12753ebc2
Juan Biancuzzo <jbiancuzzo@fi.uba.ar>
2022-03-16 14:36:30 -0300

    Creando el package de vector dinamico

---
+++ Editor/IIdentificador.cs
@@ -0,0 +1,9 @@
+﻿namespace ItIsNotOnlyMe.VectorDinamico
+{
+    public interface IIdentificador
+    {
+        public int GetID();
+
+        public bool EsIgual(IIdentificador identificador);
+    }
+}
